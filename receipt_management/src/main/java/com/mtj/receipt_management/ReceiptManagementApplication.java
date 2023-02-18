eef74f3030e8f7d4b8f1f7256edf345e2993733b
JamesLeMaitre <jamestchao14@gmail.com>
2023-02-18 12:32:15 +0000

    firdt commit

---
+++ receipt_management/src/main/java/com/mtj/receipt_management/ReceiptManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mtj.receipt_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptManagementApplication.class, args);
+    }
+
+}
---
+++ receipt_management/src/test/java/com/mtj/receipt_management/ReceiptManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtj.receipt_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
